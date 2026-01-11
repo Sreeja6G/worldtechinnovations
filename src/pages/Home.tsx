ebaa19d77d8cbe227232bcbf96c68922f27c8a26
Sreeja6G <reddysreeja038@gmail.com>
2026-01-11 21:35:13 +0530

    Update Home page styles and layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -348,7 +348,7 @@ export default function Home() {
     <p className="footer-contact">
       📧{" "}
       <a href="mailto:worldtechinnovations786@gmail.com">
-        worldtechinnovations786@gmail.com
+        yaswanthworks786@gmail.com
       </a>
     </p>
 
